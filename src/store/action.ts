382852f4aa2a4bab755c0081649d77866b53c6d3
Валерия <justlera007@gmail.com>
2024-03-17 15:56:59 +0300

    Сконфигурирует  axios, добавит redux-thunk и token

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,9 +1,11 @@
 import { createAction } from '@reduxjs/toolkit';
 import { CityMap } from '../types/city-map';
 import { SortType } from '../const';
+import { Offers } from '../types/offers';
 
 export const setCityActive = createAction('main/setCityActive', (value: string) => ({ payload: value }));
 export const getOffers = createAction('main/getOffers');
 export const setChangeMap = createAction('map/setChangeMap', (value: CityMap) => ({ payload: value }));
 export const getSortType = createAction('main/getSortType', (value: SortType) => ({ payload: value }));
 export const setSortedOffers = createAction('setSortedOffers');
+export const loadOffers = createAction<Offers>('data/loadOffers');
