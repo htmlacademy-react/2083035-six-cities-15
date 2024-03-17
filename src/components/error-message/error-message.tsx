68df1ed5519cc257731ca6a4f0d4acc4a005f5c3
Валерия <justlera007@gmail.com>
2024-03-17 17:18:01 +0300

    Создаст компонент ErrorMessage и подключит его

---
+++ src/components/error-message/error-message.tsx
@@ -0,0 +1,13 @@
+import { useAppSelector } from '../../hooks';
+import style from './error-message.module.css';
+
+
+function ErrorMessage(): JSX.Element | null {
+    const error = useAppSelector((state) => state.error);
+
+    return (error)
+        ? <div className={style.error__message}>{error}</div>
+        : null;
+}
+
+export default ErrorMessage;
